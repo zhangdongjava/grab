97629e2e57094b26bf66cb1176b44ff27dd273ce
zhangdongjava <787395919@qq.com>
2016-11-04 18:37:43 +0800

    新版本添加核心控制器

--- king/src/main/java/com/zzz/play/util/WaitNotfiy.java
+++ king/src/main/java/com/zzz/play/util/WaitNotfiy.java
@@ -13,6 +13,7 @@ public class WaitNotfiy {
         if (!wait) return;
         synchronized (lock) {
             try {
+                System.out.println("进入等待");
                 lock.wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -23,7 +24,9 @@ public class WaitNotfiy {
 
     public void anotfiy() {
         synchronized (lock) {
+            System.out.println("开始唤醒!");
             lock.notifyAll();
+            System.out.println("唤醒结束!");
         }
     }
 

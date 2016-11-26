a09cec55188a62ee46d0653ec4efebc64cc79eb0
zhangdongjava <787395919@qq.com>
2016-11-26 22:55:17 +0800

    初始化脚本分块

--- king/src/main/java/com/zzz/play/setp/copy/BaZhenTu.java
+++ king/src/main/java/com/zzz/play/setp/copy/BaZhenTu.java
@@ -10,7 +10,7 @@ import java.util.Map;
  */
 public class BaZhenTu extends FuBen {
 
-    Map<String,String> map = new HashMap<>();
+    Map<String, String> map = new HashMap<>();
 
     @Override
     public String inClearLine() {
@@ -24,32 +24,32 @@ public class BaZhenTu extends FuBen {
 
     @Override
     public String saveLine() {
-        return "";
+        return "金刚奇书,红宝石";
     }
 
     /**
      * 云阵：右上左左左左下
-     天阵：右上上上上上上
-     地阵：右下下下下下下
-     蛇阵：右下右下右下右下
-     虎阵：右下下右右右右
-     龙阵：右上上右右右右
-     风阵：右下右右上上右
-     鸟阵：右右右右下右右
+     * 天阵：右上上上上上上
+     * 地阵：右下下下下下下
+     * 蛇阵：右下右下右下右下
+     * 虎阵：右下下右右右右
+     * 龙阵：右上上右右右右
+     * 风阵：右下右右上上右
+     * 鸟阵：右右右右下右右
      */
     @Override
     public void ready() {
-        goodsTakeout.setGoods("金刚奇书_1");
+        goodsTakeout.setGoods("金刚奇书_5");
         goodsTakeout.run();
 
-        map.put("云阵","右上左左左左下");
-        map.put("天阵","右上上上上上上");
-        map.put("地阵","右下下下下下下");
-        map.put("蛇阵","右下右下右下右下右下");
-        map.put("虎阵","右下下右右右右");
-        map.put("龙阵","右上上右右右右");
-        map.put("风阵","右下右右上上右");
-        map.put("鸟阵","右右右右下右右");
+        map.put("云阵", "右上左左左左下");
+        map.put("天阵", "右上上上上上上");
+        map.put("地阵", "右下下下下下下");
+        map.put("蛇阵", "右下右下右下右下右下");
+        map.put("虎阵", "右下下右右右右");
+        map.put("龙阵", "右上上右右右右");
+        map.put("风阵", "右下右右上上右");
+        map.put("鸟阵", "右右右右下右右");
     }
 
 
@@ -64,53 +64,35 @@ public class BaZhenTu extends FuBen {
         htmlContent.linkName("右:黄泥冈→");
         htmlContent.linkName("右:悬崖→");
         inwujiang();
-        htmlContent.linkName("八阵图道士");
-        htmlContent.linkName("传送到八阵图(需金刚奇书x1)");
-        htmlContent.linkName("刷新");
-        if (htmlContent.getText().contains("今天你已进入")) {
-            ableIn = false;
-            lastDate = new Date();
-            logger.error(htmlContent.htmlPanel.user.getName()  + "八阵图副本结束!");
-            htmlContent.linkName("返回游戏");
-            return false;
+        for (int i = 0; i < 5; i++) {
+            if (!shua()) {
+               break;
+            }
         }
-        go();
-        htmlContent.linkName("阵守将",true);
-        zhanDou1();
-        htmlContent.linkName("进入中军阵");
-        zhanDou("中军守将");
-        htmlContent.linkName("进入中军人门");
-        zhanDou("中军人门守将");
-        htmlContent.linkName("进入中军地门");
-        zhanDou("中军地门守将");
-        htmlContent.linkName("进入中军天门");
-        zhanDou("中军天门守将");
-        htmlContent.linkName("进入中军阵");
-        zhanDou("中军元帅");
         outwujiang();
         return true;
     }
 
     private void go() {
         String text = htmlContent.getText();
-        if(text.length()>8){
-            text = text.substring(0,8);
+        if (text.length() > 8) {
+            text = text.substring(0, 8);
         }
         String line = null;
         for (Map.Entry<String, String> entry : map.entrySet()) {
-            if(text.contains(entry.getKey())){
+            if (text.contains(entry.getKey())) {
                 line = entry.getValue();
                 break;
             }
         }
-        if(line!= null){
+        if (line != null) {
             char[] chars = line.toCharArray();
             for (char aChar : chars) {
-                htmlContent.linkName(Character.toString(aChar)+":",true);
+                htmlContent.linkName(Character.toString(aChar) + ":", true);
                 zhanDou1();
             }
         }
-        htmlContent.linkName("阵中心",true);
+        htmlContent.linkName("阵中心", true);
     }
 
     /**
@@ -155,39 +137,66 @@ public class BaZhenTu extends FuBen {
         htmlContent.linkName("返回游戏");
     }
 
-    public void inwujiang(){
+    public void inwujiang() {
         htmlContent.linkName("返回游戏");
         htmlContent.linkName("武将");
         htmlContent.linkName("撤消近身");
         htmlContent.linkName("撤消近身");
         htmlContent.linkName("撤消近身");
         htmlContent.linkName("撤消近身");
-        htmlContent.linkName("武松",true);
+        htmlContent.linkName("武松", true);
         htmlContent.linkName("设为近身武将");
         htmlContent.linkName("我的武将");
-        htmlContent.linkName("鲁智深",true);
+        htmlContent.linkName("鲁智深", true);
         htmlContent.linkName("设为近身武将");
         htmlContent.linkName("我的武将");
-        htmlContent.linkName("周武师",true);
+        htmlContent.linkName("周武师", true);
         htmlContent.linkName("设为近身武将");
         htmlContent.linkName("我的武将");
-        htmlContent.linkName("柴进",true);
-        htmlContent.linkName("设为近身武将");
-        htmlContent.linkName("我的武将");
-        htmlContent.linkName("返回游戏");
-    }
-    public void outwujiang(){
-        htmlContent.linkName("返回游戏");
-        htmlContent.linkName("武将");
-        htmlContent.linkName("武松",true);
-        htmlContent.linkName("撤销近身武将");
-        htmlContent.linkName("我的武将");
-        htmlContent.linkName("史进",true);
+        htmlContent.linkName("柴进", true);
         htmlContent.linkName("设为近身武将");
         htmlContent.linkName("我的武将");
         htmlContent.linkName("返回游戏");
     }
 
+    public void outwujiang() {
+        htmlContent.linkName("返回游戏");
+        htmlContent.linkName("武将");
+        htmlContent.linkName("武松", true);
+        htmlContent.linkName("撤销近身武将");
+        htmlContent.linkName("我的武将");
+        htmlContent.linkName("史进", true);
+        htmlContent.linkName("设为近身武将");
+        htmlContent.linkName("我的武将");
+        htmlContent.linkName("返回游戏");
+    }
+
+    private boolean shua() {
+        htmlContent.linkName("八阵图道士");
+        htmlContent.linkName("传送到八阵图(需金刚奇书x1)");
+        htmlContent.linkName("刷新");
+        if (htmlContent.getText().contains("今天你已进入")) {
+            ableIn = false;
+            lastDate = new Date();
+            logger.error(htmlContent.htmlPanel.user.getName() + "八阵图副本结束!");
+            htmlContent.linkName("返回游戏");
+            return false;
+        }
+        go();
+        htmlContent.linkName("阵守将", true);
+        zhanDou1();
+        htmlContent.linkName("进入中军阵");
+        zhanDou("中军守将");
+        htmlContent.linkName("进入中军人门");
+        zhanDou("中军人门守将");
+        htmlContent.linkName("进入中军地门");
+        zhanDou("中军地门守将");
+        htmlContent.linkName("进入中军天门");
+        zhanDou("中军天门守将");
+        htmlContent.linkName("进入中军阵");
+        zhanDou("中军元帅");
+        return true;
+    }
 
 
 }

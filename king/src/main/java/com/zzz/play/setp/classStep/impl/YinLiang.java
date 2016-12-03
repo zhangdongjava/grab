8dd207329c0d4f9fa138c554f9ec902778d1a3fc
zhangdongjava <787395919@qq.com>
2016-12-03 17:05:20 +0800

    html换成swing

--- king/src/main/java/com/zzz/play/setp/classStep/impl/YinLiang.java
+++ king/src/main/java/com/zzz/play/setp/classStep/impl/YinLiang.java
@@ -6,6 +6,8 @@ import com.zzz.play.util.HtmlContent;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 双倍银两使用
@@ -18,11 +20,13 @@ public class YinLiang extends SecondRefresh {
     private GoodsUse goodsUse;
 
     private int num = 1;
+    private Set<String> notSave = new HashSet<>();
 
     public YinLiang(HtmlContent htmlContent) {
         this.htmlContent = htmlContent;
-        goodsUse = new GoodsUse("双倍银两卡_"+(num+1));
+        goodsUse = new GoodsUse("双倍银两卡_" + (num + 1));
         goodsUse.setHtmlContent(htmlContent);
+        notSave.add("哈嘿");
     }
 
     @Override
@@ -39,21 +43,24 @@ public class YinLiang extends SecondRefresh {
         Calendar calendar = Calendar.getInstance();
         int h = calendar.get(Calendar.HOUR_OF_DAY);
         int min = calendar.get(Calendar.MINUTE);
-        if (((h!=8&&h!=9&&h!=10))) {
+        if (((h != 8 && h != 9 && h != 10))) {
             return false;
         }
-        htmlContent.linkName("返回游戏");
-        htmlContent.linkName("功能菜单");
-        htmlContent.linkName("神行千里");
-        htmlContent.linkName("上东京");
-        htmlContent.linkName("上:北大街↑");
-        htmlContent.linkName("活动大使");
-        htmlContent.linkName("回馈点", true);
-        for (int i = 0; i < num; i++) {
-            htmlContent.linkName("倍银两卡(绑)", true);
-            htmlContent.linkName("回馈点兑换奖励");
+        if (!notSave.contains(htmlContent.htmlPanel.user.getName())) {
+            htmlContent.linkName("返回游戏");
+            htmlContent.linkName("功能菜单");
+            htmlContent.linkName("神行千里");
+            htmlContent.linkName("上东京");
+            htmlContent.linkName("上:北大街↑");
+            htmlContent.linkName("活动大使");
+            htmlContent.linkName("回馈点", true);
+            for (int i = 0; i < num; i++) {
+                htmlContent.linkName("倍银两卡(绑)", true);
+                htmlContent.linkName("回馈点兑换奖励");
+            }
+            htmlContent.linkName("返回游戏");
         }
-        htmlContent.linkName("返回游戏");
+
         goodsUse.run();
         ableIn = false;
         lastDate = new Date();

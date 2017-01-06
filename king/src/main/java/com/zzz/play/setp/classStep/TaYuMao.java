8766c46f1d31df9a1fedc3eee8ebc789239ceeff
zhangdongjava <787395919@qq.com>
2017-01-06 15:37:59 +0800

    八卦阵

--- king/src/main/java/com/zzz/play/setp/classStep/TaYuMao.java
+++ king/src/main/java/com/zzz/play/setp/classStep/TaYuMao.java
@@ -111,7 +111,7 @@ public class TaYuMao extends BaseStep {
                 if (shijian == 1) {
                     return;
                 }
-                if ((double) wujiang / (shibing + 1) >= 3) {
+                if ((double) wujiang / (shibing + 1) >= 4) {
                     formSubmit.setValue("10");
                     shibing++;
                     daZao("打造士兵防具");
